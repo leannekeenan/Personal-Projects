6a132a22033c01c23440de142e0c054deeffb8f8
Leanne Keenan <l_keenan@outlook.com>
2025-02-13 19:31:23 -0800

    creating react app for D&D module mini games

---
+++ sweet adventures club/assets/js/square.js
@@ -0,0 +1,36 @@
+const { SquareClient, SquareEnvironment, SquareError } = require("square");
+require('dotenv').config()
+
+const client = new SquareClient({
+  token: process.env.SQUARE_ACCESS_TOKEN,
+  environment: SquareEnvironment.Sandbox,
+});
+
+async function getLocations() {
+  try {
+    let listLocationsResponse = await client.locations.list();
+
+    let locations = listLocationsResponse.locations;
+
+    locations.forEach(function (location) {
+      console.log(
+        location.id + ": " +
+        location.name + ", " +
+        location.address.addressLine1 + ", " +
+        location.address.locality
+      );
+    });
+  } catch (error) {
+    if (error instanceof SquareError) {
+      error.errors.forEach(function (e) {
+        console.log(e.category);
+        console.log(e.code);
+        console.log(e.detail);
+      });
+    } else {
+      console.log("Unexpected error occurred: ", error);
+    }
+  }
+};
+
+getLocations();

35ea0bbdbc81f624ad6337232e42f2688372a811
Leanne Keenan <leanne.keenan1@gmail.com>
2026-01-15 21:20:27 -0800

    add route pathes to admin and main booking in main,jsx

--- bookings/frontend/src/main.jsx
+++ bookings/frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Dashboard from './components/Dashboard.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* This is the public booking page */}
+        <Route path="/" element={<App />} />
+        
+        {/* This is your secret admin page */}
+        <Route path="/admin" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )

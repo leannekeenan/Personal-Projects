b234b6b20548ad8c4ecb465ba86ab9327d564282
Leanne Keenan <leanne.keenan1@gmail.com>
2026-01-26 17:26:43 -0800

    Deleted 'signup' project

--- signup/backend/models/Market.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const MarketSchema = new mongoose.Schema({
-    isOpen: { type: Boolean, default: false },
-    inventory: { type: Number, default: 192 }
-});
-
-module.exports = mongoose.model('Market', MarketSchema);
